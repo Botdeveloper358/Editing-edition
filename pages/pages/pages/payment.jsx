409f455f511da3a6834aac850d94c21708e54d01
Botdeveloper358 <indonesiyaserver852101@gmail.com>
2025-07-17 22:42:50 +0530

    Create payment.jsx

---
+++ pages/pages/pages/payment.jsx
@@ -0,0 +1,10 @@
+export default function Payment() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white space-y-4">
+      <h1 className="text-4xl">💳 Payment</h1>
+      <p>UPI ID: hariomgupta8538@axl</p>
+      <p>🎞️ 1 Reel: ₹1000</p>
+      <p>📅 30 Reels Monthly Offer: ₹20000</p>
+    </div>
+  );
+}
